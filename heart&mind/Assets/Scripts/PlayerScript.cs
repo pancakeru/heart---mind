1bb61669b2785b8075ddc64d70df15d681c11e05
Zoe Chow <zoechowym22@gmail.com>
2024-10-20 16:34:07 -0400

    include intellisense?

--- heart&mind/Assets/Scripts/PlayerScript.cs
+++ heart&mind/Assets/Scripts/PlayerScript.cs
@@ -7,10 +7,15 @@ public class PlayerScript : MonoBehaviour
     //move speed var
     private int speed;
 
+    //rigidbody 2d reference
+    private Rigidbody2D rb;
+
 
     void Start()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)) {
+            
+        }
 
     }
 
