121b16cd8ecea6af927c51d99709a4c193480bc0
AntonioTheReis <antoniobalcaoreis@gmail.com>
2024-10-23 23:05:45 -0400

    Mind Mechanics almost done, only visual of bar left to implement

--- heart&mind/Assets/Scripts/MindMechanics.cs
+++ heart&mind/Assets/Scripts/MindMechanics.cs
@@ -7,6 +7,8 @@ public class MindMechanics : MonoBehaviour
 
     [SerializeField] private GameObject grower;
     [SerializeField] private float rateOfChange;
+    [SerializeField] private float initialSize;
+    private float localCount;
     private static float limit = 120f;
     private float realPosChange;
     private float realScaleChange;
@@ -19,6 +21,14 @@ public class MindMechanics : MonoBehaviour
         realScaleChange = rateOfChange * 0.129f;
 
         count = 0f;
+        localCount = 0f;
+
+        for (int i = 0; i < initialSize; i++)
+        {
+            grower.transform.position = new Vector3(grower.transform.position.x, grower.transform.position.y + realPosChange);
+            grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y + realScaleChange, grower.transform.localScale.z);
+            localCount++;
+        }
     }
 
     // Update is called once per frame
@@ -29,36 +39,24 @@ public class MindMechanics : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.tag == "Player" && Input.GetKey(KeyCode.O))
+        if(collision.tag == "Player" && Input.GetKey(KeyCode.P))
         {
             if(count < limit)
             {
                 grower.transform.position = new Vector3(grower.transform.position.x, grower.transform.position.y + realPosChange);
-                if (count >= 0)
-                {
-                    grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y + realScaleChange, grower.transform.localScale.z);
-                }
-                else
-                {
-                    grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y - realScaleChange, grower.transform.localScale.z);
-                }
+                grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y + realScaleChange, grower.transform.localScale.z);                
                 count++;
+                localCount++;
             }
         }
         if (collision.tag == "Player" && Input.GetKey(KeyCode.L))
         {
-            if(count > -limit)
+            if(count > -limit && localCount > 0)
             {
-                grower.transform.position = new Vector3(grower.transform.position.x, grower.transform.position.y - realPosChange);
-                if (count <= 0)
-                {
-                    grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y + realScaleChange, grower.transform.localScale.z);
-                }
-                else
-                {
-                    grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y - realScaleChange, grower.transform.localScale.z);
-                }
+                grower.transform.position = new Vector3(grower.transform.position.x, grower.transform.position.y - realPosChange); 
+                grower.transform.localScale = new Vector3(grower.transform.localScale.x, grower.transform.localScale.y - realScaleChange, grower.transform.localScale.z);
                 count--;
+                localCount--;
             }
         }
     }
